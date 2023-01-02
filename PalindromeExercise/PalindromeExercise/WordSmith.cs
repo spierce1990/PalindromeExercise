d5ed579731d526c350219a25bb5aba55f4820a6c
Sionylen <sionylenpierce@outlook.com>
2023-01-01 23:40:17 -0700

    added

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+
+        public bool IsPalindrome(string someWord )
+        { 
+
+        string reversedWord = "";
+
+            for (int i = someWord.Length-1; i >= 0; i--)
+            {
+                reversedWord += someWord[i];
+
+            }
+            if (reversedWord == someWord)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,24 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("mom", true)]
+        [InlineData("dad", true)]
+        [InlineData("sis", true)]
+        [InlineData("level", true)]
+        [InlineData("sister", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            //Arrange
+            var instance = new WordSmith();
+
+            //Act
+
+            var actual = instance.IsPalindrome(word);
+
+            //Assert
+
+            Assert.Equal(expected, actual);
 
         }
     }
